ea052fb3f7055a0a25eed3a40a75615d4976e65c
Mohamed Camara GUEYE <ahmed.abdbassir@gmail.com>
2016-06-14 17:15:22 +0000

    Project init

---
+++ wp_advanced_auth.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ *
+ * Plugin Name: WP Advanced Auth
+ * Description: Used by millions, Akismet is quite possibly the best way in the
+ * Author: Aicha Garmy
+ */
+
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
